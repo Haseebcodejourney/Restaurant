bd74d72af8235209fbb02cf664c71bbd482d63aa
Haseebcodejourney <hamzaneuraltec@gmail.com>
2024-11-24 22:33:03 +0200

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React from 'react';
+import '../style/header/style.css'
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <>
+        <header className='header-wrapper'>
+            <div className='left-header-wrapper'>
+                <span></span>
+                <span>
+                    <figure>
+                        <img src='https://grandrestaurantv6.b-cdn.net/grandrestaurantv6/demo10/wp-content/uploads/sites/10/2021/01/logo-white.png' alt='Logo' width={90} height={59} />
+                    </figure>
+                </span>
+            </div>
+            <div className='right-header-wrapper'>
+                <ul>
+                    <li>685-456-6743</li>
+                    <li>Order Online</li>
+                    <li>Reservation</li>
+                </ul>
+            </div>
+        </header>
+    </>
   )
 }
