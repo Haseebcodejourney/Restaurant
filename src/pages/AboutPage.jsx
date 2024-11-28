530798a901b959d0f16aa4f2defed6860789804b
Haseebcodejourney <hamzaneuraltec@gmail.com>
2024-11-28 23:33:31 +0200

    update

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Chef from '../components/Chef';
 import '../style/chef/style.css'
 import Main from '../components/Main';
+import Manager from '../components/Manager'
 
 export default function AboutPage() {
   return (
     <>
       <Main/>
       <Chef/>
+      <Manager/>
     </>
   )
 }
